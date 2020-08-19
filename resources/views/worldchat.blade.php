acdf6cbcae1de9497fad7784e91d1d154692dbe9
EricTalv <eric.talviste@tptlive.ee>
2020-08-19 20:45:04 +0300

    fixed bootstrap issue

--- resources/views/worldchat.blade.php
+++ resources/views/worldchat.blade.php
@@ -2,10 +2,18 @@
 
 @section('content')
     <div>
-        <div class="links">
+        <div class="">
             <a href="{{'/'}}">Back</a>
-            <h4>Welcome chat</h4>
+            <h4>World Chat</h4>
+            <hr>
+        </div>
+
+        <div>
+        
+        <div class="text-uppercase">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto quia atque quae tenetur iste eius deserunt sunt doloribus tempore.</div>
 
         </div>
+
     </div>
 @endsection
